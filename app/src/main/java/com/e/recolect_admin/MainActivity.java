2a33b08e7cc50b99c2325ac19595d44fa71bff8c
Alan <alanboronat182@gmail.com>
2019-08-23 14:50:44 -0300

    commit se cambiaron iconos en el navigation drawer

--- app/src/main/java/com/e/recolect_admin/MainActivity.java
+++ app/src/main/java/com/e/recolect_admin/MainActivity.java
@@ -85,7 +85,7 @@ public class MainActivity extends AppCompatActivity
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_slideshow) {
+        /*} else if (id == R.id.nav_slideshow) {*/
 
         /*} else if (id == R.id.nav_tools) {*/
 
