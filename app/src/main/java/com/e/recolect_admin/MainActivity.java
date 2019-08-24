8b41e5a528ebf3b4ef102fd61a084844391176e3
pabloaav <pablovicentin84@gmail.com>
2019-08-24 10:50:18 -0300

    quitando lineas comentadas de los items

--- app/src/main/java/com/e/recolect_admin/MainActivity.java
+++ app/src/main/java/com/e/recolect_admin/MainActivity.java
@@ -90,10 +90,6 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_tools) {
 
-       /* } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {*/
-
         }
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
