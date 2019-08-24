5fbc4bee865a7a35f35d9e6022174a4aa8d4b768
pabloaav <pablovicentin84@gmail.com>
2019-08-24 16:19:32 -0300

    cambio nombre de login admin xml

--- app/src/main/java/com/e/recolect_admin/LoginAdmin.java
+++ app/src/main/java/com/e/recolect_admin/LoginAdmin.java
@@ -9,6 +9,6 @@ public class LoginAdmin extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_login);
+        setContentView(R.layout.admin_login);
     }
 }
