12b9de673e05adb4e98b96fd8ac67ac64c6f4df1
pabloaav <pablovicentin84@gmail.com>
2019-08-29 21:33:20 -0300

    Principio de grafica de Pie o torta de incidencias

--- app/src/main/java/com/e/recolect_admin/fragmentos/ReporteIncidenciaFragment.java
+++ app/src/main/java/com/e/recolect_admin/fragmentos/ReporteIncidenciaFragment.java
@@ -1,17 +1,38 @@
 package com.e.recolect_admin.fragmentos;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 
+import com.e.recolect_admin.MainActivity;
 import com.e.recolect_admin.R;
+import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import com.github.mikephil.charting.utils.MPPointF;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,7 +42,7 @@ import com.github.mikephil.charting.charts.PieChart;
  * Use the {@link ReporteIncidenciaFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ReporteIncidenciaFragment extends Fragment {
+public class ReporteIncidenciaFragment extends Fragment implements OnChartValueSelectedListener {
 
     //region Atributos por default
     // TODO: Rename parameter arguments, choose names that match
@@ -38,6 +59,8 @@ public class ReporteIncidenciaFragment extends Fragment {
 
     //region ATRIBUTOS
     private PieChart pieIncidencias;
+    protected final String[] parties = new String[]{"Vidrio", "Domiciliario", "Industrial", "Chatarra"};
+    private int[] sale = new int[]{25, 20, 38, 10, 15};
     //endregion
 
     //region Metodos por default
@@ -88,6 +111,16 @@ public class ReporteIncidenciaFragment extends Fragment {
         mListener = null;
     }
 
+    @Override
+    public void onValueSelected(Entry e, Highlight h) {
+
+    }
+
+    @Override
+    public void onNothingSelected() {
+
+    }
+
     /**
      * This interface must be implemented by activities that contain this
      * fragment to allow an interaction in this fragment to be communicated
@@ -108,11 +141,6 @@ public class ReporteIncidenciaFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-
 
     }
 
@@ -124,8 +152,111 @@ public class ReporteIncidenciaFragment extends Fragment {
 
         //Link del recurso pieChart de Incidencias
         pieIncidencias = vista.findViewById(R.id.graf_pie_incidencias);
+        pieIncidencias.setUsePercentValues(true);
+        pieIncidencias.getDescription().setEnabled(false);
+        pieIncidencias.setExtraOffsets(5, 10, 5, 5);
+        pieIncidencias.setDragDecelerationFrictionCoef(0.95f);
 
+        pieIncidencias.setCenterText(generateCenterSpannableText());
+
+        pieIncidencias.setDrawHoleEnabled(true);
+        pieIncidencias.setHoleColor(Color.WHITE);
+
+        pieIncidencias.setTransparentCircleColor(Color.WHITE);
+        pieIncidencias.setTransparentCircleAlpha(110);
+
+        pieIncidencias.setHoleRadius(58f);
+        pieIncidencias.setTransparentCircleRadius(61f);
+
+        pieIncidencias.setDrawCenterText(true);
+
+        pieIncidencias.setRotationAngle(0);
+        // enable rotation of the chart by touch
+        pieIncidencias.setRotationEnabled(true);
+        pieIncidencias.setHighlightPerTapEnabled(true);
+        pieIncidencias.animateY(1400, Easing.EaseInOutQuad);
+
+        Legend l = pieIncidencias.getLegend();
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
+        l.setOrientation(Legend.LegendOrientation.VERTICAL);
+        l.setDrawInside(false);
+        l.setXEntrySpace(7f);
+        l.setYEntrySpace(0f);
+        l.setYOffset(0f);
+
+        // entry label styling
+        pieIncidencias.setEntryLabelColor(Color.WHITE);
+        pieIncidencias.setEntryLabelTextSize(12f);
+        pieIncidencias.animateY(1400, Easing.EaseInOutQuad);
+        setData();
         return vista;
+    }
+
+    private SpannableString generateCenterSpannableText() {
+
+        SpannableString s = new SpannableString("Recolect-Admin\nReporte de Incidencias");
+        s.setSpan(new RelativeSizeSpan(1.7f), 0, 14, 0);
+        s.setSpan(new StyleSpan(Typeface.NORMAL), 14, s.length() - 15, 0);
+        s.setSpan(new ForegroundColorSpan(Color.GRAY), 14, s.length() - 15, 0);
+        s.setSpan(new RelativeSizeSpan(.8f), 14, s.length() - 15, 0);
+        s.setSpan(new StyleSpan(Typeface.ITALIC), s.length() - 14, s.length(), 0);
+        s.setSpan(new ForegroundColorSpan(ColorTemplate.getHoloBlue()), s.length() - 14, s.length(), 0);
+        return s;
+    }
+
+    private void setData() {
+
+
+        PieDataSet dataSet = new PieDataSet(getPieEntries(), "Tipos de Incidencia");
+
+        dataSet.setDrawIcons(false);
+
+        dataSet.setSliceSpace(3f);
+        dataSet.setIconsOffset(new MPPointF(0, 40));
+        dataSet.setSelectionShift(5f);
+
+        // add a lot of colors
+
+        ArrayList<Integer> colors = new ArrayList<>();
+
+        for (int c : ColorTemplate.VORDIPLOM_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.JOYFUL_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.COLORFUL_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.LIBERTY_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.PASTEL_COLORS)
+            colors.add(c);
+
+        colors.add(ColorTemplate.getHoloBlue());
+
+        dataSet.setColors(colors);
+        //dataSet.setSelectionShift(0f);
+
+        PieData data = new PieData(dataSet);
+        data.setValueFormatter(new PercentFormatter(pieIncidencias));
+        data.setValueTextSize(18f);
+        data.setValueTextColor(Color.WHITE);
+        pieIncidencias.setData(data);
+
+        // undo all highlights
+        pieIncidencias.highlightValues(null);
+
+        pieIncidencias.invalidate();
+    }
+
+    private ArrayList<PieEntry> getPieEntries() {
+        ArrayList<PieEntry> entries = new ArrayList<>();
+        for (int i = 0; i < sale.length; i++)
+            entries.add(new PieEntry(sale[i]));
+        return entries;
     }
     //endregion
 }
